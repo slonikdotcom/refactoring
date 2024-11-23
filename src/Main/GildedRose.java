28b8788cfeef6d3499e2043503725abe236a3861
Toma <tamara.podpalaya@gmail.com>
2024-11-23 09:45:12 +0200

    The updateQuality method is split into separate methods for each logic(1,7)

--- src/Main/GildedRose.java
+++ src/Main/GildedRose.java
@@ -8,55 +8,73 @@ public class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    private void updateItemQuality(Item item) {
+        if (isLegendaryItem(item)) {
+            return;
+        }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+        updateQualityForItem(item);
+        updateSellInForItem(item);
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+        if (item.sellIn < 0) {
+            handleExpiredItem(item);
+        }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    private boolean isLegendaryItem(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private void updateQualityForItem(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            updateBackstagePassQuality(item);
+        } else {
+            decreaseQuality(item);
+        }
+    }
+
+    private void updateBackstagePassQuality(Item item) {
+        increaseQuality(item);
+
+        if (item.sellIn < 11) {
+            increaseQuality(item);
+        }
+
+        if (item.sellIn < 6) {
+            increaseQuality(item);
+        }
+    }
+
+    private void handleExpiredItem(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+        } else {
+            decreaseQuality(item);
+        }
+    }
+
+    private void updateSellInForItem(Item item) {
+        item.sellIn--;
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
         }
     }
 }
